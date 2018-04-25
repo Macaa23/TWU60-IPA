13c44967701252414110dc774b84080cc991fd5f
Rocio Sepulveda <rocio_23s@hotmail.com>
2018-04-25 09:57:38 -0300

    added vertical line and right triangle

--- src/TriangleExercises.java
+++ src/TriangleExercises.java
@@ -8,7 +8,13 @@ public class TriangleExercises {
         System.out.println(triangleExercises.easiestExerciseEver()+"\n");
 
         System.out.println("Draw A Horizontal Line:");
-        System.out.println(triangleExercises.drawAHorizontalLine(3));
+        System.out.println(triangleExercises.drawAHorizontalLine(3)+"\n");
+
+        System.out.println("Draw A Vertical Line:");
+        System.out.println(triangleExercises.drawAVerticalLine(3)+"\n");
+
+        System.out.println("Draw A Right Triangle:");
+        System.out.println(triangleExercises.drawARightTriangle(3)+"\n");
 
     }
 
@@ -20,6 +26,28 @@ public class TriangleExercises {
         String line = "";
         for (int i = 0; i < n ; i++){
             line +="*";
+        }
+
+        return line;
+    }
+
+    public String drawAVerticalLine(int n){
+        String line = "";
+        for (int i = 0; i < n ; i++){
+            line +="*\n";
+        }
+
+        return line;
+    }
+
+    public String drawARightTriangle(int n){
+        String line = "";
+        for (int i = 0; i <= n ; i++){
+            for (int j = 0; j < i; j++){
+                line+= "*";
+            }
+
+            line+= "\n";
         }
 
         return line;
